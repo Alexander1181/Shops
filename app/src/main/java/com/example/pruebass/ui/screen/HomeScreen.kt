65f9e90b9ffe906036ad978f3320c95be0ac61de
SSDD <al.tortella@duocuc.cl>
2026-01-19 16:53:52 -0300

    hice la confi del mnsj De welcome y que tambien muestre una img

--- app/src/main/java/com/example/pruebass/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pruebass/ui/screen/HomeScreen.kt
@@ -1,16 +1,25 @@
 package com.example.pruebass.ui.screen
 
+import android.widget.Button
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.pruebass.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,8 +36,27 @@ fun HomeScreen(){
                 .padding(15.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-
+            Text(text = "Welcome a la mia Applicatione")
+            Image(
+                painter = painterResource(id = R.drawable.rene),
+                contentDescription = "Imagen App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+            Button(
+                onClick = {}
+            ){
+                Text("Haz clic aqui")
+            }
         }
     }
+}
 
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
 }
